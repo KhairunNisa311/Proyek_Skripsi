d1e27d5036c982b1e26ce14e692903710e8d0f9e
KhairunNisa311 <ginazoom01@gmail.com>
2024-06-30 18:54:25 +0700

    update home

--- home.php
+++ home.php
@@ -1,9 +1,10 @@
 <h2>FUZZY ANALYTICAL HIERARCY PROCESS (FAHP)</h2>
-<p>Terdapat banyak literatur yang menyebutkan ketidaktepatan keputusan
-dalam penggunaan perbandingan rasio. Secara umum kebanyakan
-manusia tidak dapat membuat perkiraan kuantitatf. Ketidakjelasan
-keputusan pilihan membuatn ketidakkonsistenan dalam menetapkan
-keputusan. </p>
+<p>Sistem ini merupakan sistem pendukung keputusan yang menggunakan metode Fuzzy AHP untuk menentukan 
+perangkingan skala prioritas dalam menentukan pemenuhan sarana dan prasarana destinasi wisata di Kabupaten Sumenep. 
+Sistem ini diharapkan dapat membantu pemerintah dalam merumuskan kebijakan yang tepat, 
+memungkinkan pengukuran skala prioritas kebutuhan sarana dan prasarana destinasi wisata di Kabupaten Sumenep dengan lebih efektif dan efisien </p>
+<p>Pada sistem ini juga terdapat langkah langkah proses perhitungan metode Fuzzy AHP. Langkah-langkah ini dilakukan untuk memastikan bahwa 
+pengambilan keputusan terkait kebijakan infrastruktur wisata dilakukan dengan akurat dan efisien</p>
 
 <div class="line"></div>
 
